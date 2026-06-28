ed167f63fe2b037d405ac6c1e26850f529b3fc6a
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-28 12:33:17 -0500

    feat: Se inicializa proyecto de Singleton

---
+++ src/main/java/com/patron/singleton/controller/EstadisticasController.java
@@ -0,0 +1,5 @@
+package com.patron.singleton.controller;
+
+public class EstadisticasController {
+    
+}
---
+++ src/main/java/com/patron/singleton/service/EstadisticasService.java
@@ -0,0 +1,8 @@
+package com.patron.singleton.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EstadisticasService {
+    
+}
