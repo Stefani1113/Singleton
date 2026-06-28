fb04d50ec1cb8fb2edac57d12e38ea75a5fdc188
Stefani1113 <stefaniapuerta604@gmail.com>
2026-06-28 12:37:20 -0500

    feat: Se implemento EstadisticasService

--- src/main/java/com/patron/singleton/service/EstadisticasService.java
+++ src/main/java/com/patron/singleton/service/EstadisticasService.java
@@ -4,5 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EstadisticasService {
+    private int totalConsultas;
+
+    public void incrementarConsultas() {
+        totalConsultas++;
+    }
     
+    public int obtenerTotalConsultas() {
+        return totalConsultas;
+    }
 }
